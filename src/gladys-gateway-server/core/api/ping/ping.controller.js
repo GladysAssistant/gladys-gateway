13e08bc23168f7b8dbc1ed4c875c49760f32736a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-09-24 08:41:34 +0800

    gladys-gateway-server: Add apidoc documentation

--- src/gladys-gateway-server/core/api/ping/ping.controller.js
+++ src/gladys-gateway-server/core/api/ping/ping.controller.js
@@ -1,5 +1,17 @@
 module.exports = function(pingModel) {
 
+  /**
+   * @api {get} /ping Return server status 
+   * @apiName ping
+   * @apiGroup Ping
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * HTTP/1.1 200 OK
+   * 
+   * {
+   *   "status": 200
+   * }
+   */
   async function ping(req, res, next) {
     await pingModel.ping();
     return res.json({status: 200});

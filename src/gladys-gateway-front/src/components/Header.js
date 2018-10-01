2375c721720f864ced75a64527888cab72f79337
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-01 10:34:13 +0800

    Layout + Header working

--- src/gladys-gateway-front/src/components/Header.js
+++ src/gladys-gateway-front/src/components/Header.js
@@ -4,60 +4,19 @@ const Header = ({ children, ...props }) => (
       <div class="container">
         <div class="d-flex">
           <a class="header-brand" href="./index.html">
-            <img src="./demo/brand/tabler.svg" class="header-brand-img" alt="tabler logo" />
+            {false && <img src="./demo/brand/tabler.svg" class="header-brand-img" alt="tabler logo" /> }
+            <span  class="header-brand-img">Gladys Gateway</span>
           </a>
           <div class="d-flex order-lg-2 ml-auto">
-            <div class="dropdown d-none d-md-flex">
-              <a class="nav-link icon" data-toggle="dropdown">
-                <i class="fe fe-bell" />
-                <span class="nav-unread" />
-              </a>
-              <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                <a href="#" class="dropdown-item d-flex">
-                  <span
-                    class="avatar mr-3 align-self-center"
-                    style="background-image: url(demo/faces/male/41.jpg)"
-                  />
-                  <div>
-                    <strong>Nathan</strong> pushed new commit: Fix page load performance issue.
-                    <div class="small text-muted">10 minutes ago</div>
-                  </div>
-                </a>
-                <a href="#" class="dropdown-item d-flex">
-                  <span
-                    class="avatar mr-3 align-self-center"
-                    style="background-image: url(demo/faces/female/1.jpg)"
-                  />
-                  <div>
-                    <strong>Alice</strong> started new task: Tabler UI design.
-                    <div class="small text-muted">1 hour ago</div>
-                  </div>
-                </a>
-                <a href="#" class="dropdown-item d-flex">
-                  <span
-                    class="avatar mr-3 align-self-center"
-                    style="background-image: url(demo/faces/female/18.jpg)"
-                  />
-                  <div>
-                    <strong>Rose</strong> deployed new version of NodeJS REST Api V3
-                    <div class="small text-muted">2 hours ago</div>
-                  </div>
-                </a>
-                <div class="dropdown-divider" />
-                <a href="#" class="dropdown-item text-center text-muted-dark">
-                  Mark all as read
-                </a>
-              </div>
-            </div>
-            <div class="dropdown">
-              <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
-                <span class="avatar" style="background-image: url(./demo/faces/female/25.jpg)" />
+            <div class={'dropdown' + (props.showDropDown && ' show')}>
+              <a onClick={props.toggleDropDown} class="nav-link pr-0 leading-none" data-toggle="dropdown">
+                <span class="avatar" style={'background-image: url(' + ( '/assets/images/undraw_profile_pic.svg' || props.user.profile_url) + ')'} />
                 <span class="ml-2 d-none d-lg-block">
-                  <span class="text-default">Jane Pearson</span>
+                  <span class="text-default">{props.user.name}</span>
                   <small class="text-muted d-block mt-1">Administrator</small>
                 </span>
               </a>
-              <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+              <div class={'dropdown-menu dropdown-menu-right dropdown-menu-arrow' + (props.showDropDown && ' show') }>
                 <a class="dropdown-item" href="#">
                   <i class="dropdown-icon fe fe-user" /> Profile
                 </a>
@@ -75,37 +34,29 @@ const Header = ({ children, ...props }) => (
             </div>
           </div>
           <a
-            href="#"
             class="header-toggler d-lg-none ml-3 ml-lg-0"
             data-toggle="collapse"
             data-target="#headerMenuCollapse"
+            onClick={props.toggleCollapsedMenu}
           >
             <span class="header-toggler-icon" />
           </a>
         </div>
       </div>
     </div>
-    <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+    <div class={'header collapse d-lg-flex p-0 ' + ( props.showCollapsedMenu && ' show')} id="headerMenuCollapse">
       <div class="container">
         <div class="row align-items-center">
-          <div class="col-lg-3 ml-auto">
-            <form class="input-icon my-3 my-lg-0">
-              <input
-                type="search"
-                class="form-control header-search"
-                placeholder="Search&hellip;"
-                tabindex="1"
-              />
-              <div class="input-icon-addon">
-                <i class="fe fe-search" />
-              </div>
-            </form>
-          </div>
           <div class="col-lg order-lg-first">
             <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
               <li class="nav-item">
-                <a href="/dashboard" class="nav-link">
+                <a href="/dashboard" class="nav-link active">
                   <i class="fe fe-home" /> Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/dashboard/instances" class="nav-link">
+                  <i class="fe fe-server" /> My Instance
                 </a>
               </li>
             </ul>
--- src/gladys-gateway-front/src/components/Layout.js
+++ src/gladys-gateway-front/src/components/Layout.js
@@ -1,14 +1,42 @@
+import { Component } from 'preact';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({ children, ...props }) => (
-  <div class="page">
-    <div class="page-main">
-      <Header />
-      <div class="my-3 my-md-5">{children}</div>
-      <Footer />
-    </div>
-  </div>
-);
+class Layout extends Component {
+  
+  state = {
+    showDropDown: false,
+    showCollapsedMenu: false
+  };
+
+  toggleDropDown = () => {
+    this.setState({ showDropDown: !this.state.showDropDown });
+  };
+
+  toggleCollapsedMenu = () => {
+    this.setState({ showCollapsedMenu: !this.state.showCollapsedMenu });
+  };
+
+  componentDidMount = () => {
+   
+  };
+
+  render(props, { showDropDown, showCollapsedMenu }) {
+    return (
+      <div class="page">
+        <div class="page-main" >
+          <Header user={props.user}
+            showDropDown={showDropDown}
+            toggleDropDown={this.toggleDropDown}
+            showCollapsedMenu={showCollapsedMenu}
+            toggleCollapsedMenu={this.toggleCollapsedMenu}
+          />
+          <div class="my-3 my-md-5">{props.children}</div>
+        </div>
+        <Footer user={props.user} />
+      </div>
+    );
+  }
+}
 
 export default Layout;

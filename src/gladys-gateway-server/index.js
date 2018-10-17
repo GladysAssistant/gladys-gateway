f2350110a48a20db8c7e7356e6aa751205c5b49f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-17 17:10:25 +0800

    add database migration at server startup

--- src/gladys-gateway-server/index.js
+++ src/gladys-gateway-server/index.js
@@ -1,2 +1,14 @@
 require('dotenv').config();
-require('./core/index')();
+
+(async() => {
+
+  // we perform database migration if needed
+  const DBMigrate = require('db-migrate');
+  const dbmigrate = DBMigrate.getInstance(true, {
+    env: (process.env.NODE_ENV === 'production') ? 'production' : 'dev' 
+  });
+  await dbmigrate.up();
+
+  // then, we start server
+  require('./core/index')();
+})();

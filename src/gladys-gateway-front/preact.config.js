748a106f0e466162b9c5fd09fc53b9ee04fbdc5a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-17 16:42:30 +0800

    Make frontend build work with Docker

--- src/gladys-gateway-front/preact.config.js
+++ src/gladys-gateway-front/preact.config.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const { SERVER_URL , STRIPE_API_KEY } = process.env;
 const webpack = require('webpack');
 
+const asyncPlugin = require('preact-cli-plugin-fast-async');
+
 module.exports = function (config) {
+  asyncPlugin(config);
+
   config.plugins.push(
     new webpack.DefinePlugin({
       'process.env.SERVER_URL': JSON.stringify(SERVER_URL),

59e97d597cf073b7b0be1fc4325c29ba41b87d32
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-22 11:41:34 +0800

    Docker build frontend with configuration

--- src/gladys-gateway-front/config.js
+++ src/gladys-gateway-front/config.js
@@ -1,13 +1,12 @@
 
 const local = {
   serverUrl: process.env.SERVER_URL,
-  accessTokenCookieKey: 'gladys-gateway-access-token',
-  refreshTokenCookieKey: 'gladys-gateway-refresh-token',
   stripeApiKey: process.env.STRIPE_API_KEY
 };
 
 const prod = {
-  serverUrl: ''
+  serverUrl: process.env.SERVER_URL,
+  stripeApiKey: process.env.STRIPE_API_KEY
 };
 
 const config = process.env.NODE_ENV === 'production' ? prod : local;

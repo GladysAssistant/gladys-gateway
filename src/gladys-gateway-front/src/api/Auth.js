f1ac4c36ccddd49a93eea03575b13411b1299675
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-22 13:53:38 +0800

    wrap all keyValStore inside promise.all to ensure data is saved

--- src/gladys-gateway-front/src/api/Auth.js
+++ src/gladys-gateway-front/src/api/Auth.js
@@ -73,14 +73,13 @@ const Auth = {
   },
   saveTwoFactorAccessToken: token => keyValStore.set('two_factor_access_token', token),
   getTwoFactorAccessToken: token => keyValStore.get('two_factor_access_token'),
-  saveLoginInformations: data => {
-    keyValStore.set('refresh_token', data.refreshToken);
-    keyValStore.set('access_token', data.accessToken);
-    keyValStore.set('device_id', data.deviceId);
-
-    keyValStore.set('rsa_keys', data.rsaKeys);
-    keyValStore.set('ecdsa_keys', data.ecdsaKeys);
-  },
+  saveLoginInformations: data => Promise.all([
+    keyValStore.set('refresh_token', data.refreshToken),
+    keyValStore.set('access_token', data.accessToken),
+    keyValStore.set('device_id', data.deviceId),
+    keyValStore.set('rsa_keys', data.rsaKeys),
+    keyValStore.set('ecdsa_keys', data.ecdsaKeys)
+  ]),
   saveUser: user => keyValStore.set('user', user),
   getUser: async () => {
     let user = await keyValStore.get('user');

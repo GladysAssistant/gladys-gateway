f4eac198d22026bb1c208e5188fc413621adf28f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-11-13 10:45:38 +0800

    Add default language on emails on server side

--- src/gladys-gateway-server/core/service/mailgun.js
+++ src/gladys-gateway-server/core/service/mailgun.js
@@ -1,6 +1,8 @@
 const Promise = require('bluebird');
 const mailgun = require('mailgun.js');
 
+const SUPPORTED_LANGUAGE = ['en', 'fr'];
+
 module.exports = function mailgunService(logger) {
 
   const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
@@ -8,7 +10,13 @@ module.exports = function mailgunService(logger) {
 
   function send(user, template, scope) {
 
+    // default language = en
+    if (SUPPORTED_LANGUAGE.indexOf(user.language) === -1) {
+      user.language = 'en';
+    }
+
     if (!emails[template] || !emails[template][user.language]) {
+      logger.info(`Invalid template or language. Template = "${template}", language = "${user.language}."`);
       return Promise.reject(new Error('INVALID_TEMPLATE_OR_LANGUAGE'));
     }
 

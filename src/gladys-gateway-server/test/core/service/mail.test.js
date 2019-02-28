851766783fc3eac290302420bd2fed9f7d4edbb3
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-28 18:05:51 +0700

    Update tests

--- src/gladys-gateway-server/test/core/service/mail.test.js
+++ src/gladys-gateway-server/test/core/service/mail.test.js
@@ -1,10 +1,11 @@
 const logger = require('tracer').colorConsole();
 const Mail = require('../../../core/service/mail');
 
-describe.skip('mail', () => {
+describe.skip('mail', function send() {
+  this.timeout(10000);
   it('should send email', () => {
     const mail = Mail(logger);
-    return mail.send({ email: 'tony.stark@gladysassistant.com', language: 'en' }, 'confirmation', {
+    return mail.send({ email: 'hello@gladysassistant.com', language: 'en' }, 'confirmation', {
       confirmationUrl: 'https://gladysassistant.com',
     });
   });

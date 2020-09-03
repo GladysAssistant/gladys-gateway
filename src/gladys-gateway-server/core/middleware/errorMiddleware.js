8bbc39c5e6129eb37b1a786eaccf890abf274c12
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-09-03 17:22:52 +0200

    Fix error in error middleware

--- src/gladys-gateway-server/core/middleware/errorMiddleware.js
+++ src/gladys-gateway-server/core/middleware/errorMiddleware.js
@@ -10,13 +10,13 @@ const {
 
 module.exports = function getErrorMiddleware(errorService) {
   return function ErrorMiddleware(error, req, res, next) {
-    errorService.track('ERROR_MIDDLEWARE', { error, path: req.route.path, user: req.user && req.user.id });
+    errorService.track('ERROR_MIDDLEWARE', { error, path: req.route && req.route.path, user: req.user && req.user.id });
     if (
-      error instanceof ValidationError
-      || error instanceof AlreadyExistError
-      || error instanceof NotFoundError
-      || error instanceof ForbiddenError
-      || error instanceof UnauthorizedError
+      error instanceof ValidationError ||
+      error instanceof AlreadyExistError ||
+      error instanceof NotFoundError ||
+      error instanceof ForbiddenError ||
+      error instanceof UnauthorizedError
     ) {
       return res.status(error.getStatus()).json(error.jsonError());
     }
--- src/gladys-gateway-server/core/service/beforeSendSentry.js
+++ src/gladys-gateway-server/core/service/beforeSendSentry.js
@@ -1,9 +1,21 @@
 const omitDeep = require('omit-deep');
 
-const SENSIBLE_FIELDS = ['alt', 'batt', 'conn', 'lat', 'lon', 'tid', 'tst', 'vac', 'vel'];
+const PROPERTIES_TO_OMIT = [
+  'password',
+  'latitude',
+  'longitude',
+  'accuracy',
+  'altitude',
+  'device_battery',
+  'lon',
+  'lat',
+  'acc',
+  'alt',
+  'batt',
+];
 
 function beforeSend(event) {
-  return omitDeep(event, SENSIBLE_FIELDS);
+  return omitDeep(event, PROPERTIES_TO_OMIT);
 }
 
 module.exports = beforeSend;

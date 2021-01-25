9c0a2d97ed3ba9e7f479144a333d22a43b6d298f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-01-25 18:08:51 +0100

    Fix condition to send data to instrumental agent

--- src/gladys-gateway-server/core/service/instrumentalAgent.js
+++ src/gladys-gateway-server/core/service/instrumentalAgent.js
@@ -3,7 +3,7 @@ const I = require('instrumental-agent');
 module.exports = function InstrumentalAgentService(logger) {
   I.configure({
     apiKey: process.env.INSTRUMENTAL_AGENT_API_KEY,
-    enabled: process.env.INSTRUMENTAL_AGENT_API_KEY && process.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === 'production',
   });
   async function sendMetric(name, value) {
     try {

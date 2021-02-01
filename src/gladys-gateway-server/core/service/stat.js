5bb6335af5b0196f12a2b5cdf9d0dad072fae554
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-02-01 17:05:14 +0100

    Send logs in batch only

--- src/gladys-gateway-server/core/service/stat.js
+++ src/gladys-gateway-server/core/service/stat.js
@@ -1,15 +1,29 @@
+const { Batcher } = require('bottleneck');
+
 module.exports = async function StatService(logger, statDb) {
-  async function track(eventName, data) {
+  const batcher = new Batcher({
+    maxTime: 5 * 1000, // every 5 seconds flush to DB
+    maxSize: 200,
+  });
+  batcher.on('batch', async (rows) => {
     if (statDb) {
       try {
-        await statDb.t_stat.insert({
-          event_type: eventName,
-          payload: data,
-        });
+        await statDb.t_stat.insert(rows);
       } catch (e) {
         logger.warn('Unable to save stat in DB');
         logger.warn(e);
       }
+    }
+  });
+  async function track(eventName, data) {
+    try {
+      batcher.add({
+        event_type: eventName,
+        payload: data,
+      });
+    } catch (e) {
+      logger.warn('Unable to add event to batch');
+      logger.warn(e);
     }
   }
 

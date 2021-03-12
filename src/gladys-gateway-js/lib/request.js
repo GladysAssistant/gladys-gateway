963894a20627ccb62f2e732ca000c1bbbc230be8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-03-12 11:03:44 +0800

    set maxBodyLength + maxContentLength to Infinity in axios upload function

--- src/gladys-gateway-js/lib/request.js
+++ src/gladys-gateway-js/lib/request.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 async function getAccessToken(state) {
   const route = state.isInstance ? '/instances/access-token' : '/users/access-token';
 
-  state.accessToken = (await axios.get(state.serverUrl + route, {
-    headers: {
-      authorization: state.refreshToken,
-    },
-  })).data.access_token;
+  state.accessToken = (
+    await axios.get(state.serverUrl + route, {
+      headers: {
+        authorization: state.refreshToken,
+      },
+    })
+  ).data.access_token;
 }
 
 function post(url, data, state) {
@@ -85,6 +87,8 @@ function upload(url, form, onUploadProgress, state) {
         authorization: state.accessToken,
         'Content-Type': form.getHeaders()['content-type'],
       },
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
       onUploadProgress,
     })
     .then((result) => result.data)

850667308c04139e445317ff162adfffb2acca53
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-05-13 16:44:46 +0800

    Add more variables to dev-email-server

--- src/gladys-gateway-server/core/common/dev-email-server.js
+++ src/gladys-gateway-server/core/common/dev-email-server.js
@@ -8,6 +8,10 @@ app.get('/:template_name/:language', (req, res) => {
   res.send(
     templates[req.params.template_name][req.params.language].ejs({
       confirmationUrlGladys4: 'http://gladysassistant.com',
+      nameOfAdminInviting: 'Tony',
+      invitationUrlGladys4: 'http://gladysassistant.com',
+      resetPasswordUrlGladys4: 'http://gladysassistant.com',
+      updateCardLink: 'http://gladysassistant.com',
     }),
   );
 });

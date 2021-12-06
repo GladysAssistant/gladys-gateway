7e33a90d0e451298e8901e164099fce33a06c512
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-12-06 15:51:12 +0100

    First poc alexa integration

---
+++ src/gladys-gateway-server/core/api/alexa/alexa.controller.js
@@ -0,0 +1,147 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
+const get = require('get-value');
+const { BadRequestError } = require('../../common/error');
+
+const VALID_REDIRECT_URIS = [
+  'https://pitangui.amazon.com/api/skill/link/M1CD0NOTQVDMUV',
+  'https://layla.amazon.com/api/skill/link/M1CD0NOTQVDMUV',
+  'https://alexa.amazon.co.jp/api/skill/link/M1CD0NOTQVDMUV',
+];
+
+module.exports = function AlexaController(
+  logger,
+  alexaModel,
+  socketModel,
+  instanceModel,
+  userModel,
+  deviceModel,
+  instrumentalAgentService,
+  errorService,
+) {
+  /**
+   * @api {post} /v1/api/alexa/smart_home Entrypoint for alexa smart home
+   * @apiName Get data/control home
+   * @apiGroup Alexa
+   */
+  async function smartHome(req, res) {
+    instrumentalAgentService.increment('backend.requests.alexa.smart-home');
+    const user = await userModel.getMySelf({ id: req.user.id });
+    const primaryInstance = await instanceModel.getPrimaryInstanceByAccount(user.account_id);
+
+    const message = {
+      version: '1.0',
+      type: 'gladys-open-api',
+      action: 'alexa-request',
+      instance_id: primaryInstance.id,
+      data: req.body,
+    };
+
+    try {
+      // then, we sent the request to the local Gladys instance
+      const response = await socketModel.sendMessageOpenApi(user, message);
+      return res.json(response);
+    } catch (e) {
+      errorService.track('ALEXA_SMART_HOME_ERROR', {
+        error: e,
+        payload: req.body,
+        user: user.id,
+      });
+
+      return res.status(404).json(e);
+    }
+  }
+  /**
+   * @api {post} /alexa/authorize Get authorization code
+   * @apiName Get authorization code
+   * @apiGroup Alexa
+   */
+  async function authorize(req, res) {
+    instrumentalAgentService.increment('backend.requests.alexa.authorize');
+    const { ALEXA_OAUTH_CLIENT_ID } = process.env;
+    if (req.body.client_id !== ALEXA_OAUTH_CLIENT_ID) {
+      throw new BadRequestError('client_id is not matching');
+    }
+    const baseUrlFound = VALID_REDIRECT_URIS.find(
+      (redirectUriBaseUrl) => req.body.redirect_uri && req.body.redirect_uri.startsWith(redirectUriBaseUrl),
+    );
+    if (!baseUrlFound) {
+      throw new BadRequestError('invalid redirect_uri');
+    }
+    const code = await alexaModel.getCode(req.user.id);
+    const redirectUrl = `${req.body.redirect_uri}?state=${req.body.state}&code=${code}`;
+    res.json({
+      redirectUrl,
+    });
+  }
+  /**
+   * @api {post} /v1/api/alexa/token Get access token
+   * @apiName Get access token
+   * @apiGroup Alexa
+   */
+  async function token(req, res, next) {
+    instrumentalAgentService.increment('backend.requests.alexa.token');
+    const { ALEXA_OAUTH_CLIENT_ID, ALEXA_OAUTH_CLIENT_SECRET } = process.env;
+    try {
+      if (req.body.client_id !== ALEXA_OAUTH_CLIENT_ID) {
+        throw new BadRequestError('client_id is not matching');
+      }
+      if (req.body.client_secret !== ALEXA_OAUTH_CLIENT_SECRET) {
+        throw new BadRequestError('client_secret is not matching');
+      }
+      if (req.body.grant_type === 'authorization_code') {
+        const { accessToken, refreshToken } = await alexaModel.getRefreshTokenAndAccessToken(req.body.code);
+        res.json({
+          token_type: 'Bearer',
+          access_token: accessToken,
+          refresh_token: refreshToken,
+          expires_in: 3600,
+        });
+      } else if (req.body.grant_type === 'refresh_token') {
+        const { accessToken } = await alexaModel.getAccessToken(req.body.refresh_token);
+        res.json({
+          token_type: 'Bearer',
+          access_token: accessToken,
+          expires_in: 3600,
+        });
+      } else {
+        throw new BadRequestError('wrong grand_type');
+      }
+    } catch (e) {
+      logger.error(e);
+      res.status(400).json({ error: 'invalid_grant' });
+    }
+  }
+  /**
+   * @api {post} /alexa/request_sync Request Sync
+   * @apiName Request Sync
+   * @apiGroup Alexa
+   */
+  async function requestSync(req, res) {
+    instrumentalAgentService.increment('backend.requests.alexa.request-sync');
+    await alexaModel.requestSync(req.instance.id);
+    res.json({
+      status: 200,
+    });
+  }
+  /**
+   * @api {post} /alexa/report_state Report State
+   * @apiName Report State
+   * @apiGroup Alexa
+   */
+  async function reportState(req, res) {
+    instrumentalAgentService.increment('backend.requests.alexa.report-state');
+    await alexaModel.reportState(req.instance.id, req.body);
+    res.json({
+      status: 200,
+    });
+  }
+  return {
+    smartHome,
+    authorize,
+    token,
+    requestSync,
+    reportState,
+  };
+};
---
+++ src/gladys-gateway-server/core/api/alexa/alexa.model.js
@@ -0,0 +1,158 @@
+const Promise = require('bluebird');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
+const randomBytes = Promise.promisify(require('crypto').randomBytes);
+const { ForbiddenError } = require('../../common/error');
+
+const ALEXA_OAUTH_CODE_REDIS_PREFIX = `ALEXA_OAUTH_CODE`;
+const ALEXA_CODE_EXPIRY_IN_SECONDS = 60 * 60;
+
+const JWT_AUDIENCE = 'alexa-oauth';
+const SCOPE = ['alexa'];
+
+const cleanNullProperties = (obj) =>
+  Object.entries(obj)
+    .map(([k, v]) => [k, v && typeof v === 'object' ? cleanNullProperties(v) : v])
+    // eslint-disable-next-line
+    .reduce((a, [k, v]) => (v == null ? a : ((a[k] = v), a)), {});
+
+module.exports = function AlexaModel(logger, db, redisClient, jwtService, errorService) {
+  const { ALEXA_OAUTH_CLIENT_ID } = process.env;
+
+  async function getRefreshTokenAndAccessToken(code) {
+    const userId = await redisClient.getAsync(`${ALEXA_OAUTH_CODE_REDIS_PREFIX}:${code}`);
+    if (userId === null) {
+      throw new ForbiddenError('INVALID_CODE');
+    }
+    const user = await db.t_user.findOne(
+      {
+        id: userId,
+      },
+      {
+        fields: ['id', 'gladys_4_user_id'],
+      },
+    );
+
+    const newDevice = {
+      id: uuid.v4(),
+      name: 'Alexa',
+      client_id: ALEXA_OAUTH_CLIENT_ID,
+      user_id: user.id,
+    };
+
+    const refreshToken = jwtService.generateRefreshTokenOauth(user, SCOPE, newDevice.id, JWT_AUDIENCE);
+    const accessToken = jwtService.generateAccessTokenOauth(user, newDevice, SCOPE, JWT_AUDIENCE);
+    newDevice.refresh_token_hash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+    await db.t_device.insert(newDevice);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async function getAccessToken(refreshToken) {
+    let userId;
+    try {
+      const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET, {
+        audience: JWT_AUDIENCE,
+        issuer: 'gladys-gateway',
+      });
+      userId = decoded.user_id;
+    } catch (e) {
+      logger.debug(e);
+      throw new ForbiddenError();
+    }
+
+    const refreshTokenHash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+
+    // we are looking for devices not revoked with
+    // this refresh_token_hash
+    const device = await db.t_device.findOne({
+      user_id: userId,
+      refresh_token_hash: refreshTokenHash,
+      revoked: false,
+      is_deleted: false,
+    });
+
+    // the device doesn't exist or has been revoked
+    if (device === null) {
+      logger.debug(`Forbidden: Refresh token not found in DB`);
+      throw new ForbiddenError();
+    }
+
+    // we get the current user account, to be sure the account is active
+    const fullUser = await db.t_user.findOne({
+      id: userId,
+      is_deleted: false,
+    });
+
+    // the user doesn't exist or has been revoked
+    if (fullUser === null) {
+      logger.debug(`Forbidden: User not found or revoked`);
+      throw new ForbiddenError();
+    }
+
+    const accessToken = jwtService.generateAccessTokenOauth(fullUser, device, SCOPE, JWT_AUDIENCE);
+
+    return {
+      accessToken,
+    };
+  }
+
+  async function getCode(userId) {
+    // we generate a random code
+    const code = (await randomBytes(64)).toString('hex');
+    // we save the code in Redis
+    await redisClient.setAsync(`${ALEXA_OAUTH_CODE_REDIS_PREFIX}:${code}`, userId, 'EX', ALEXA_CODE_EXPIRY_IN_SECONDS);
+    return code;
+  }
+
+  const getUsersWithAlexaActivatedQuery = `
+      SELECT DISTINCT t_user.id, t_user.account_id
+      FROM t_user
+      INNER JOIN t_device ON t_user.id = t_device.user_id
+      INNER JOIN t_instance ON t_user.account_id = t_instance.account_id
+      WHERE t_instance.id = $1
+      AND t_device.revoked = false
+      AND t_device.is_deleted = false
+      AND t_device.client_id = $2;
+    `;
+
+  async function requestSync(instanceId) {
+    const users = await db.query(getUsersWithAlexaActivatedQuery, [instanceId, ALEXA_OAUTH_CLIENT_ID]);
+    if (users.length > 0) {
+      // request sync
+    }
+  }
+
+  async function reportState(instanceId, payload) {
+    const users = await db.query(getUsersWithAlexaActivatedQuery, [instanceId, ALEXA_OAUTH_CLIENT_ID]);
+    if (users.length > 0) {
+      const payloadCleaned = cleanNullProperties(payload);
+      const request = {
+        requestId: uuid.v4(),
+        agentUserId: users[0].account_id,
+        payload: payloadCleaned,
+      };
+      try {
+        // report state
+      } catch (e) {
+        errorService.track('ALEXA_REPORT_STATE_ERROR', {
+          error: e,
+          payload: payloadCleaned,
+          user: users[0].id,
+        });
+      }
+    }
+  }
+
+  return {
+    getRefreshTokenAndAccessToken,
+    getAccessToken,
+    getCode,
+    requestSync,
+    reportState,
+  };
+};

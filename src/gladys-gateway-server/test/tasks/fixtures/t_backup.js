d9492052d1e6924323d72346e06859a1865131bc
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-05-06 17:36:58 +0100

    Update fixtures

--- src/gladys-gateway-server/test/tasks/fixtures/t_backup.js
+++ src/gladys-gateway-server/test/tasks/fixtures/t_backup.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     id: '74dc8d58-3997-484a-a791-53e5b07279d7',
-    path: 'https://test.test-endpoint.com/un-autre-backup.enc',
+    path: `https://${process.env.STORAGE_BUCKET}.${process.env.STORAGE_ENDPOINT}/un-autre-backup.enc`,
     size: 1000,
     account_id: 'b2d23f66-487d-493f-8acb-9c8adb400def',
     created_at: '2018-10-16T02:21:25.901Z',
@@ -9,7 +9,7 @@ module.exports = [
   },
   {
     id: '72543b45-246b-49fc-be16-d728c46d7060',
-    path: 'https://test.test-endpoint.com/un-backup.enc',
+    path: `https://${process.env.STORAGE_BUCKET}.${process.env.STORAGE_ENDPOINT}/un-backup.enc`,
     size: 1000,
     account_id: 'be2b9666-5c72-451e-98f4-efca76ffef54',
     created_at: '2018-10-16T02:21:25.901Z',

f07e69a5d866ba19af98e6144430635a7aac65de
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-09-09 10:26:25 +0200

    Add test on 500 errors

--- src/gladys-gateway-server/test/core/api/enedis/enedis.controller.enedisAPI.test.js
+++ src/gladys-gateway-server/test/core/api/enedis/enedis.controller.enedisAPI.test.js
@@ -179,6 +179,71 @@ describe('Enedis API', () => {
           .expect('Content-Type', /json/)
           .expect(403);
       });
+      it('should retry 500 error and return 200', async () => {
+        // First, finalize Enedis Oauth process
+        nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
+          .post('/v1/oauth2/token', (body) => {
+            const grandTypeValid = body.grant_type === 'authorization_code';
+            const codeValid = body.code === 'someAuthCode';
+            const clientIdValid = body.client_id === process.env.ENEDIS_GRANT_CLIENT_ID;
+            const clientSecretValid = body.client_secret === process.env.ENEDIS_GRANT_CLIENT_SECRET;
+            return grandTypeValid && codeValid && clientIdValid && clientSecretValid;
+          })
+          .reply(200, {
+            access_token: 'ba42fe5a-0eaa-11e5-9813-4dd05b3a25f3',
+            token_type: 'Bearer',
+            expires_in: 12600,
+            refresh_token: '7dnCbf8P0ypCyxbnX7tUKjcSveE2Nu8w',
+            scope: '/v3/metering_data/consumption_load_curve.GET',
+            issued_at: '1487075532179',
+            refresh_token_issued_at: '1487075532179',
+            usage_points_id: '16401220101758,16401220101710,16401220101720',
+            apigo_client_id: '73cd2d7f-e361-b7f6-48359493ed2c',
+          });
+        await request(TEST_BACKEND_APP)
+          .post('/enedis/finalize')
+          .send({
+            code: 'someAuthCode',
+          })
+          .set('Accept', 'application/json')
+          .set('Authorization', configTest.jwtAccessTokenDashboard)
+          .expect('Content-Type', /json/)
+          .expect(200);
+        // Then, send first request
+        nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
+          .post('/v1/oauth2/token', (body) => {
+            const grandTypeValid = body.grant_type === 'refresh_token';
+            const refreshTokenValid = body.refresh_token === '7dnCbf8P0ypCyxbnX7tUKjcSveE2Nu8w';
+            const clientIdValid = body.client_id === process.env.ENEDIS_GRANT_CLIENT_ID;
+            const clientSecretValid = body.client_secret === process.env.ENEDIS_GRANT_CLIENT_SECRET;
+            return grandTypeValid && refreshTokenValid && clientIdValid && clientSecretValid;
+          })
+          .reply(200, {
+            access_token: 'ba42fe5a-0eaa-11e5-9813-4dd05b3a25f3',
+            token_type: 'Bearer',
+            expires_in: 12600,
+            refresh_token: '7dnCbf8P0ypCyxbnX7tUKjcSveE2Nu8w',
+            scope: '/v3/metering_data/consumption_load_curve.GET',
+            issued_at: '1487075532179',
+            refresh_token_issued_at: '1487075532179',
+            usage_points_id: '16401220101758,16401220101710,16401220101720',
+            apigo_client_id: '73cd2d7f-e361-b7f6-48359493ed2c',
+          });
+        nock(`https://${process.env.ENEDIS_BACKEND_URL}`).get(enedisRoute).reply(500);
+        nock(`https://${process.env.ENEDIS_BACKEND_URL}`).get(enedisRoute).reply(200, data);
+        const response = await request(TEST_BACKEND_APP)
+          .get(gladysPlusRoute)
+          .query({
+            usage_point_id: 16401220101758,
+            start: '2022-08-01',
+            end: '2022-08-03',
+          })
+          .set('Accept', 'application/json')
+          .set('Authorization', configTest.jwtAccessTokenInstance)
+          .expect('Content-Type', /json/)
+          .expect(200);
+        expect(response.body).to.deep.equal(data);
+      });
       it('should return 403', async () => {
         await request(TEST_BACKEND_APP)
           .get(gladysPlusRoute)

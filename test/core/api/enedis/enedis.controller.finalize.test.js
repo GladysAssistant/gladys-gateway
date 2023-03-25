5f165610053dcc02c5a60946b6c79b2076528504
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-03-25 10:41:57 +0800

    Enedis: Adapt authorize process for production (#138)

--- test/core/api/enedis/enedis.controller.finalize.test.js
+++ test/core/api/enedis/enedis.controller.finalize.test.js
@@ -1,28 +1,9 @@
 const request = require('supertest');
 const { expect } = require('chai');
-const nock = require('nock');
 const configTest = require('../../../tasks/config');
 
 describe('POST /enedis/finalize', () => {
   it('should save refresh token in DB and return list of usage_points_id', async () => {
-    nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
-      .post('/oauth2/v3/token', (body) => {
-        const grandTypeValid = body.grant_type === 'authorization_code';
-        const codeValid = body.code === 'someAuthCode';
-        const clientIdValid = body.client_id === process.env.ENEDIS_GRANT_CLIENT_ID;
-        const clientSecretValid = body.client_secret === process.env.ENEDIS_GRANT_CLIENT_SECRET;
-        return grandTypeValid && codeValid && clientIdValid && clientSecretValid;
-      })
-      .reply(200, {
-        access_token: 'ba42fe5a-0eaa-11e5-9813-4dd05b3a25f3',
-        token_type: 'Bearer',
-        expires_in: 12600,
-        refresh_token: '7dnCbf8P0ypCyxbnX7tUKjcSveE2Nu8w',
-        scope: '/v3/metering_data/consumption_load_curve.GET',
-        issued_at: '1487075532179',
-        refresh_token_issued_at: '1487075532179',
-        apigo_client_id: '73cd2d7f-e361-b7f6-48359493ed2c',
-      });
     const response = await request(TEST_BACKEND_APP)
       .post('/enedis/finalize')
       .send({
@@ -36,45 +17,5 @@ describe('POST /enedis/finalize', () => {
     expect(response.body).to.deep.equal({
       usage_points_id: ['16401220101758', '16401220101710', '16401220101720'],
     });
-  });
-  it('should not save refresh token in DB, invalid code', async () => {
-    nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
-      .post('/oauth2/v3/token', (body) => {
-        const grandTypeValid = body.grant_type === 'authorization_code';
-        const codeValid = body.code === 'someAuthCode';
-        const clientIdValid = body.client_id === process.env.ENEDIS_GRANT_CLIENT_ID;
-        const clientSecretValid = body.client_secret === process.env.ENEDIS_GRANT_CLIENT_SECRET;
-        return grandTypeValid && codeValid && clientIdValid && clientSecretValid;
-      })
-      .reply(403);
-    await request(TEST_BACKEND_APP)
-      .post('/enedis/finalize')
-      .send({
-        code: 'someAuthCode',
-      })
-      .set('Accept', 'application/json')
-      .set('Authorization', configTest.jwtAccessTokenDashboard)
-      .expect('Content-Type', /json/)
-      .expect(403);
-  });
-  it('should return 500 internal server error', async () => {
-    nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
-      .post('/oauth2/v3/token', (body) => {
-        const grandTypeValid = body.grant_type === 'authorization_code';
-        const codeValid = body.code === 'someAuthCode';
-        const clientIdValid = body.client_id === process.env.ENEDIS_GRANT_CLIENT_ID;
-        const clientSecretValid = body.client_secret === process.env.ENEDIS_GRANT_CLIENT_SECRET;
-        return grandTypeValid && codeValid && clientIdValid && clientSecretValid;
-      })
-      .reply(500);
-    await request(TEST_BACKEND_APP)
-      .post('/enedis/finalize')
-      .send({
-        code: 'someAuthCode',
-      })
-      .set('Accept', 'application/json')
-      .set('Authorization', configTest.jwtAccessTokenDashboard)
-      .expect('Content-Type', /json/)
-      .expect(500);
   });
 });

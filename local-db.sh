60e9dad0518178ac90da6627f57a07d0f1fe9346
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-04-06 11:11:36 +0800

    Improve Enedis errors debugging (#143)

--- local-db.sh
+++ local-db.sh
@@ -6,5 +6,5 @@ docker rm -v redis-gladysgateway
 docker stop postgres-gladysgateway
 docker rm -v postgres-gladysgateway
 
-docker run --name postgres-gladysgateway -e POSTGRES_PASSWORD=postgres -p 5432:5432  -d postgres:14 
+docker run --name postgres-gladysgateway -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=gladysgatewaytest -p 5432:5432  -d postgres:14 
 docker run --name redis-gladysgateway -p 6379:6379 -d redis:7

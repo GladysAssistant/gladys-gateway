c7f8979c11b00b0ef21fae5e777bbdf8cc25e49d
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-04-14 17:28:29 +0800

    Add test for 404 on file

--- test/core/api/camera/camera.controller.test.js
+++ test/core/api/camera/camera.controller.test.js
@@ -26,4 +26,16 @@ describe('cameraController', () => {
       .set('Authorization', configTest.jwtAccessTokenDashboard);
     expect(response.body.toString()).to.equal('test');
   });
+  it('should return 404 not found', async function Test() {
+    this.timeout(10000);
+    const response = await request(TEST_BACKEND_APP)
+      .get('/cameras/11ff9014-6fa5-473c-8f38-0d798ba977bf/0f7fcced-6c7c-4cf9-b6a0-18ad50f6033a')
+      .set('Authorization', configTest.jwtAccessTokenDashboard)
+      .expect(404);
+    expect(response.body).to.deep.equal({
+      status: 404,
+      error_code: 'NOT_FOUND',
+      error_message: 'File not found',
+    });
+  });
 });

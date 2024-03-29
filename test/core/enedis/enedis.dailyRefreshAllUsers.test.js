395e19197034d2814f3177d9be547c62f6cd092f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2024-03-29 09:28:13 +0100

    Enedis: Better catch error when user consent is no longer found

--- test/core/enedis/enedis.dailyRefreshAllUsers.test.js
+++ test/core/enedis/enedis.dailyRefreshAllUsers.test.js
@@ -43,6 +43,39 @@ describe('EnedisWorker.dailyRefreshAllUsers', function Describe() {
     await shutdown();
   });
   it('should publish 2 jobs', async () => {
+    // Insert broken enedis user data
+    await TEST_DATABASE_INSTANCE.t_account.insert({
+      id: 'ab9c205a-d090-4c97-84b5-d2a9eb932201',
+      name: 'user_with_broken_enedis_sync@test.fr',
+      current_period_end: new Date('2050-11-19T16:00:00.000Z'),
+      status: 'active',
+    });
+    await TEST_DATABASE_INSTANCE.t_user.insert({
+      id: '1258b0b1-4f5a-4ea6-926b-755ab725eeb3',
+      email_confirmation_token_hash: 'dlkflmdskflmdskfmldskfkdsfldsfldksmfkdslfksdf',
+      email: 'user_with_broken_enedis_sync@test.fr',
+      email_confirmed: true,
+      srp_salt: 'ddd',
+      srp_verifier: 'sdfsdf',
+      two_factor_enabled: false,
+      role: 'admin',
+      account_id: 'ab9c205a-d090-4c97-84b5-d2a9eb932201',
+      language: 'fr',
+      name: 'Broken Enedis',
+    });
+    await TEST_DATABASE_INSTANCE.t_device.insert({
+      id: '002a93b7-e21b-48bb-a414-29bcc31615ba',
+      user_id: '1258b0b1-4f5a-4ea6-926b-755ab725eeb3',
+      client_id: process.env.ENEDIS_GRANT_CLIENT_ID,
+      name: 'Enedis',
+      refresh_token_hash: 'a43fc595dfbf78cf52e644cd30e4cb45ad1f4ef2472c6819cf4546afdece8888',
+      created_at: '2024-10-16T02:21:25.901Z',
+      last_seen: '2024-10-16T02:21:25.901Z',
+    });
+    await TEST_DATABASE_INSTANCE.t_enedis_usage_point.insert({
+      account_id: 'ab9c205a-d090-4c97-84b5-d2a9eb932201',
+      usage_point_id: 'broken-usage-point',
+    });
     // First, finalize Enedis Oauth process
     nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
       .post('/oauth2/v3/token', (body) => {
@@ -63,10 +96,18 @@ describe('EnedisWorker.dailyRefreshAllUsers', function Describe() {
         usage_points_id: '16401220101758,16401220101710,16401220101720',
         apigo_client_id: '73cd2d7f-e361-b7f6-48359493ed2c',
       });
+
     nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
       .get('/customers_upc/v5/usage_points/contracts')
       .query(contractQueryParams)
       .reply(200, contractData);
+    nock(`https://${process.env.ENEDIS_BACKEND_URL}`)
+      .get('/customers_upc/v5/usage_points/contracts')
+      .query({ usage_point_id: 'broken-usage-point' })
+      .reply(403, {
+        error: 'ERRE001150',
+        error_description: 'No consent can be found for this customer and this usage point.',
+      });
     await request(TEST_BACKEND_APP)
       .post('/enedis/finalize')
       .send({
@@ -77,6 +118,7 @@ describe('EnedisWorker.dailyRefreshAllUsers', function Describe() {
       .set('Authorization', configTest.jwtAccessTokenDashboard)
       .expect('Content-Type', /json/)
       .expect(200);
+    mockAccessTokenRefresh();
     mockAccessTokenRefresh();
     await enedisModel.dailyRefreshOfAllUsers();
     const counts = await enedisModel.queue.getJobCounts('wait', 'completed', 'failed');

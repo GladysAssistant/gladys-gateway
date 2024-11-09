35e6c8a24cb78bae1107e26785fd5d0cc7d42acb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2024-11-09 10:44:00 +0100

    Sentry: remove custom error to prevent sentry flood

--- core/api/instance/instance.model.js
+++ core/api/instance/instance.model.js
@@ -124,7 +124,7 @@ module.exports = function InstanceModel(logger, db, redisClient, jwtService, fin
 
     // the instance doesn't exist or has been deleted
     if (instanceInDb === null) {
-      throw new ForbiddenError(`Instance ${instance.id} doesn't exist or has been deleted.`);
+      throw new ForbiddenError();
     }
 
     const accessToken = jwtService.generateAccessTokenInstance(instanceInDb);
